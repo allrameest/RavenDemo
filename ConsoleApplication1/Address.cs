e4806a3030ecfa7a60d691df90b0272a0f2bff67
Erik Juhlin <erik@juhlin.nu>
2012-01-20 10:29:29 +0100

    Some code

---
+++ ConsoleApplication1/Address.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApplication1
+{
+    public class Address
+    {
+        public string Street { get; set; }
+
+        public string City { get; set; }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ConsoleApplication1
+﻿using System.Collections.ObjectModel;
+using Raven.Client.Document;
+
+namespace ConsoleApplication1
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
+            var store = new DocumentStore();
 
+            using (var session = store.OpenSession())
+            {
+                var customer = new Customer
+                                   {
+                                       FirstName = "Erik",
+                                       LastName = "Juhlin",
+                                       Email = "erik@juhlin.nu",
+                                       AcceptNewsletter = true,
+                                       Addresses = new Collection<Address>
+                                                       {
+                                                           new Address
+                                                               {
+                                                                   Street = "Helmfeltsgatan 7",
+                                                                   City = "Helsingborg"
+                                                               }
+                                                       }
+                                   };
+
+                session.Store(customer);
+                session.SaveChanges();
+            }
         }
     }
 }
