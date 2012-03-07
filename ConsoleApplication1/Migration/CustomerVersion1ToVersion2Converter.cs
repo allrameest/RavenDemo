8726746cc321d0e390c978896c352b8d6ed0eca4
Erik Juhlin <erik@juhlin.nu>
2012-03-07 08:05:07 +0100

    Changed CustomerVersion1ToVersion2Converter

--- ConsoleApplication1/Migration/CustomerVersion1ToVersion2Converter.cs
+++ ConsoleApplication1/Migration/CustomerVersion1ToVersion2Converter.cs
@@ -8,18 +8,25 @@ namespace ConsoleApplication1.Migration
 	{
 		public void EntityToDocument(object entity, RavenJObject document, RavenJObject metadata)
 		{
+			//var c = entity as Customer;
+			//if (c == null)
+			//{
+			//    return;
+			//}
+
+			//metadata["Customer-Schema-Version"] = 2;
 		}
 
 		public void DocumentToEntity(object entity, RavenJObject document, RavenJObject metadata)
 		{
-            //var c = entity as Customer;
-            //if (c == null || metadata.Value<int>("Customer-Schema-Version") >= 2)
-            //{
-            //    return;
-            //}
+			//var c = entity as Customer;
+			//if (c == null || metadata.Value<int>("Customer-Schema-Version") >= 2)
+			//{
+			//    return;
+			//}
 
-            //c.Name = document.Value<string>("FirstName") + " " + document.Value<string>("LastName");
-            //c.Email = document.Value<string>("Email");
+			//c.Name = document.Value<string>("FirstName") + " " + document.Value<string>("LastName");
+			//c.Email = document.Value<string>("Email");
 		}
 	}
 }
