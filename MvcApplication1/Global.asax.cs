64e2790c40e4c84fe49fd94f072b61507fd534dc
Erik Juhlin <erik@juhlin.nu>
2013-04-23 22:41:08 +0200

    More cleanup :)

--- MvcApplication1/Global.asax.cs
+++ MvcApplication1/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -9,16 +6,12 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using Autofac;
 using Autofac.Integration.Mvc;
-using Raven.Abstractions.Data;
 using Raven.Client;
 using Raven.Client.Document;
 
 namespace MvcApplication1
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
